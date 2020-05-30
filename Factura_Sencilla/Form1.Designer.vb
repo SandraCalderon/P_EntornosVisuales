e955d483672bba3cf85be9b6f3ccf5693a2af7c5
SandraCalderon <sandra21jackelin@gmail.com>
2020-05-29 19:36:24 -0600

    Factura sencilla completa!!

---
+++ Factura_Sencilla/Form1.Designer.vb
@@ -0,0 +1,307 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
+        Me.btnSalir = New System.Windows.Forms.Button()
+        Me.btnNuevo = New System.Windows.Forms.Button()
+        Me.btnCalcular = New System.Windows.Forms.Button()
+        Me.txtTotal = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.txtISV = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtDescuento = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtsubtotal = New System.Windows.Forms.TextBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtCantidad = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.cmbISV = New System.Windows.Forms.ComboBox()
+        Me.chkISV = New System.Windows.Forms.CheckBox()
+        Me.cmbDescuento = New System.Windows.Forms.ComboBox()
+        Me.chkDescuento = New System.Windows.Forms.CheckBox()
+        Me.txtPrecio = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'btnSalir
+        '
+        Me.btnSalir.Location = New System.Drawing.Point(259, 143)
+        Me.btnSalir.Name = "btnSalir"
+        Me.btnSalir.Size = New System.Drawing.Size(75, 32)
+        Me.btnSalir.TabIndex = 8
+        Me.btnSalir.Text = "Salir"
+        Me.ToolTip1.SetToolTip(Me.btnSalir, "Abandona la aplicacion")
+        Me.btnSalir.UseVisualStyleBackColor = True
+        '
+        'btnNuevo
+        '
+        Me.btnNuevo.Enabled = False
+        Me.btnNuevo.Location = New System.Drawing.Point(259, 88)
+        Me.btnNuevo.Name = "btnNuevo"
+        Me.btnNuevo.Size = New System.Drawing.Size(75, 35)
+        Me.btnNuevo.TabIndex = 7
+        Me.btnNuevo.Text = "Nuevo"
+        Me.ToolTip1.SetToolTip(Me.btnNuevo, "Crea una nueva factura")
+        Me.btnNuevo.UseVisualStyleBackColor = True
+        '
+        'btnCalcular
+        '
+        Me.btnCalcular.Location = New System.Drawing.Point(259, 32)
+        Me.btnCalcular.Name = "btnCalcular"
+        Me.btnCalcular.Size = New System.Drawing.Size(75, 35)
+        Me.btnCalcular.TabIndex = 6
+        Me.btnCalcular.Text = "Calcular"
+        Me.btnCalcular.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText
+        Me.ToolTip1.SetToolTip(Me.btnCalcular, "Realiza los calculos")
+        Me.btnCalcular.UseVisualStyleBackColor = True
+        '
+        'txtTotal
+        '
+        Me.txtTotal.Enabled = False
+        Me.txtTotal.Location = New System.Drawing.Point(78, 102)
+        Me.txtTotal.Name = "txtTotal"
+        Me.txtTotal.Size = New System.Drawing.Size(100, 20)
+        Me.txtTotal.TabIndex = 7
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(12, 106)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(31, 13)
+        Me.Label6.TabIndex = 6
+        Me.Label6.Text = "Total"
+        '
+        'txtISV
+        '
+        Me.txtISV.Enabled = False
+        Me.txtISV.Location = New System.Drawing.Point(78, 76)
+        Me.txtISV.Name = "txtISV"
+        Me.txtISV.Size = New System.Drawing.Size(100, 20)
+        Me.txtISV.TabIndex = 5
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(12, 80)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(24, 13)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "ISV"
+        '
+        'txtDescuento
+        '
+        Me.txtDescuento.Enabled = False
+        Me.txtDescuento.Location = New System.Drawing.Point(78, 50)
+        Me.txtDescuento.Name = "txtDescuento"
+        Me.txtDescuento.Size = New System.Drawing.Size(100, 20)
+        Me.txtDescuento.TabIndex = 3
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(12, 54)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(59, 13)
+        Me.Label4.TabIndex = 2
+        Me.Label4.Text = "Descuento"
+        '
+        'txtsubtotal
+        '
+        Me.txtsubtotal.BackColor = System.Drawing.SystemColors.InactiveBorder
+        Me.txtsubtotal.Enabled = False
+        Me.txtsubtotal.Location = New System.Drawing.Point(78, 24)
+        Me.txtsubtotal.Name = "txtsubtotal"
+        Me.txtsubtotal.Size = New System.Drawing.Size(100, 20)
+        Me.txtsubtotal.TabIndex = 1
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.txtTotal)
+        Me.GroupBox2.Controls.Add(Me.Label6)
+        Me.GroupBox2.Controls.Add(Me.txtISV)
+        Me.GroupBox2.Controls.Add(Me.Label5)
+        Me.GroupBox2.Controls.Add(Me.txtDescuento)
+        Me.GroupBox2.Controls.Add(Me.Label4)
+        Me.GroupBox2.Controls.Add(Me.txtsubtotal)
+        Me.GroupBox2.Controls.Add(Me.Label3)
+        Me.GroupBox2.Location = New System.Drawing.Point(18, 215)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(213, 146)
+        Me.GroupBox2.TabIndex = 9
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Detalle Factura"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 28)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(46, 13)
+        Me.Label3.TabIndex = 0
+        Me.Label3.Text = "Subtotal"
+        '
+        'txtCantidad
+        '
+        Me.txtCantidad.Location = New System.Drawing.Point(81, 50)
+        Me.txtCantidad.Name = "txtCantidad"
+        Me.txtCantidad.Size = New System.Drawing.Size(100, 20)
+        Me.txtCantidad.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(22, 53)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(52, 13)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Cantidad:"
+        '
+        'cmbISV
+        '
+        Me.cmbISV.Enabled = False
+        Me.cmbISV.FormattingEnabled = True
+        Me.cmbISV.Items.AddRange(New Object() {"15%", "17%"})
+        Me.cmbISV.Location = New System.Drawing.Point(41, 159)
+        Me.cmbISV.Name = "cmbISV"
+        Me.cmbISV.Size = New System.Drawing.Size(121, 21)
+        Me.cmbISV.TabIndex = 5
+        Me.cmbISV.Text = "LISTADO 2 OPCIONES"
+        '
+        'chkISV
+        '
+        Me.chkISV.AutoSize = True
+        Me.chkISV.Location = New System.Drawing.Point(39, 131)
+        Me.chkISV.Name = "chkISV"
+        Me.chkISV.Size = New System.Drawing.Size(78, 17)
+        Me.chkISV.TabIndex = 4
+        Me.chkISV.Text = "Aplicar ISV"
+        Me.chkISV.UseVisualStyleBackColor = True
+        '
+        'cmbDescuento
+        '
+        Me.cmbDescuento.Enabled = False
+        Me.cmbDescuento.FormattingEnabled = True
+        Me.cmbDescuento.Items.AddRange(New Object() {"5%", "10%", "15%", "20%", "30%", "Tercera Edad "})
+        Me.cmbDescuento.Location = New System.Drawing.Point(41, 104)
+        Me.cmbDescuento.Name = "cmbDescuento"
+        Me.cmbDescuento.Size = New System.Drawing.Size(121, 21)
+        Me.cmbDescuento.TabIndex = 3
+        Me.cmbDescuento.Text = "LISTADO 6 TIPOS DE DESCUENTO"
+        '
+        'chkDescuento
+        '
+        Me.chkDescuento.AutoSize = True
+        Me.chkDescuento.Location = New System.Drawing.Point(39, 76)
+        Me.chkDescuento.Name = "chkDescuento"
+        Me.chkDescuento.Size = New System.Drawing.Size(113, 17)
+        Me.chkDescuento.TabIndex = 2
+        Me.chkDescuento.Text = "Aplicar Descuento"
+        Me.chkDescuento.UseVisualStyleBackColor = True
+        '
+        'txtPrecio
+        '
+        Me.txtPrecio.Location = New System.Drawing.Point(81, 20)
+        Me.txtPrecio.Name = "txtPrecio"
+        Me.txtPrecio.Size = New System.Drawing.Size(100, 20)
+        Me.txtPrecio.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(22, 23)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(59, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "P. Unitario:"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.txtCantidad)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.cmbISV)
+        Me.GroupBox1.Controls.Add(Me.chkISV)
+        Me.GroupBox1.Controls.Add(Me.cmbDescuento)
+        Me.GroupBox1.Controls.Add(Me.chkDescuento)
+        Me.GroupBox1.Controls.Add(Me.txtPrecio)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(219, 187)
+        Me.GroupBox1.TabIndex = 5
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Datos Requeridos"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(356, 388)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.btnSalir)
+        Me.Controls.Add(Me.btnNuevo)
+        Me.Controls.Add(Me.btnCalcular)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Factura Sencilla"
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents ToolTip1 As ToolTip
+    Friend WithEvents btnSalir As Button
+    Friend WithEvents btnNuevo As Button
+    Friend WithEvents btnCalcular As Button
+    Friend WithEvents txtTotal As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents txtISV As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtDescuento As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtsubtotal As TextBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtCantidad As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents cmbISV As ComboBox
+    Friend WithEvents chkISV As CheckBox
+    Friend WithEvents cmbDescuento As ComboBox
+    Friend WithEvents chkDescuento As CheckBox
+    Friend WithEvents txtPrecio As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+End Class
---
+++ Factura_Sencilla/Form1.vb
@@ -0,0 +1,157 @@
+﻿Public Class Form1
+    Private Sub btnCalcular_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCalcular.Click
+        'DECLARACION DE VARIABLES
+        Dim pUnitario, cantidad As Integer
+        Dim descuento, subTotal, total, ISV As Double
+        Dim aplicarDescuento, aplicarISV As Double
+
+        'VALIDAR VARIABLES
+        pUnitario = Val(txtPrecio.Text)
+        cantidad = Val(txtCantidad.Text)
+
+        'VALIDACIONES
+
+        If txtPrecio.Text = "" Then
+            MsgBox("Debe ingresar el P. Unitario", vbInformation)
+            txtPrecio.Focus()
+            Exit Sub
+        End If
+        If txtCantidad.Text = "" Then
+            MsgBox("Debe ingresar la cantidad", vbInformation)
+            txtCantidad.Focus()
+            Exit Sub
+        End If
+        If chkDescuento.Checked = True And cmbDescuento.SelectedIndex = -1 Then
+            MsgBox("Debe seleccionar el valor del descuento a aplicar", 64, "ATENCION")
+            cmbDescuento.Focus()
+            Exit Sub
+        End If
+        If chkISV.Checked = True And cmbISV.SelectedIndex = -1 Then
+            MsgBox("Debe seleccionar el valor del ISV a aplicar", 64, "ATENCION")
+            chkISV.Focus()
+            Exit Sub
+        End If
+        If Not IsNumeric(txtCantidad.Text) Then
+            MsgBox("Ingrese un valor numerico en cantidad", vbInformation)
+            txtCantidad.Focus()
+        ElseIf txtCantidad.Text < 0 Then
+            MsgBox("No se aceptan numeros negativos!!!", vbInformation)
+            txtCantidad.Focus()
+        End If
+        If Not IsNumeric(txtPrecio.Text) Then
+            MsgBox("Ingrese un valor numerico en P. Unitario", vbInformation)
+            txtPrecio.Focus()
+        ElseIf txtPrecio.Text < 0 Then
+            MsgBox("No se aceptan numeros negativos!!!", vbInformation)
+            txtPrecio.Focus()
+        End If
+
+
+        'PROCEDIMIENTO
+        aplicarDescuento = chkDescuento.Checked
+        descuento = 0
+
+        subTotal = pUnitario * cantidad
+        If aplicarDescuento = True Then
+            Select Case cmbDescuento.SelectedIndex
+                Case 0
+                    descuento = subTotal * 0.05
+                Case 1
+                    descuento = subTotal * 0.1
+                Case 2
+                    descuento = subTotal * 0.15
+                Case 3
+                    descuento = subTotal * 0.2
+                Case 4
+                    descuento = subTotal * 0.3
+                Case 5
+                    descuento = subTotal * 0.4
+            End Select
+
+        End If
+
+
+        aplicarISV = chkISV.Checked
+        ISV = 0
+
+        If aplicarISV = True Then
+            Select Case cmbISV.SelectedIndex
+                Case 0
+                    ISV = subTotal * 0.15
+                Case 1
+                    ISV = subTotal * 0.17
+            End Select
+
+        End If
+
+
+
+        'SALIDA
+        total = subTotal - descuento + ISV
+
+        txtsubtotal.Text = subTotal
+        txtDescuento.Text = descuento
+        txtISV.Text = ISV
+        txtTotal.Text = total
+
+        btnNuevo.Enabled = True
+
+    End Sub
+
+    Private Sub btnSalir_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnSalir.Click
+        Dim salir As String
+
+        salir = MsgBox("¿Esta seguro que desea salir?", 36, "SALIR")
+        If salir <> 6 Then
+            txtPrecio.Focus()
+        Else
+            End
+        End If
+    End Sub
+
+    Private Sub chkDescuento_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles chkDescuento.CheckedChanged
+
+        If chkDescuento.Checked = True Then
+            cmbDescuento.Enabled = True
+            chkISV.Enabled = False
+        Else
+            cmbDescuento.Enabled = False
+            chkISV.Enabled = True
+            cmbDescuento.SelectedIndex = -1
+        End If
+
+
+
+
+    End Sub
+
+    Private Sub cmbISV_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmbISV.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub btnNuevo_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnNuevo.Click
+        txtCantidad.Clear()
+        txtPrecio.Clear()
+        txtDescuento.Clear()
+        txtsubtotal.Clear()
+        txtISV.Clear()
+        txtTotal.Clear()
+        chkDescuento.Checked = False
+        chkISV.Checked = False
+        cmbDescuento.SelectedIndex = -1
+        cmbISV.SelectedIndex = -1
+        txtPrecio.Focus()
+        btnNuevo.Enabled = False
+    End Sub
+
+    Private Sub chkISV_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles chkISV.CheckedChanged
+        If chkISV.Checked = True Then
+            cmbISV.Enabled = True
+            chkDescuento.Enabled = False
+        Else
+            cmbISV.Enabled = False
+            chkDescuento.Enabled = True
+            cmbISV.SelectedIndex = -1
+        End If
+    End Sub
+End Class
