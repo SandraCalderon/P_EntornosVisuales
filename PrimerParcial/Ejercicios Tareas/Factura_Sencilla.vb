55604d760ad1de65884ac2a0167b855021181ea1
SandraCalderon <sandra21jackelin@gmail.com>
2020-05-29 19:16:29 -0600

    Se agrego formulario Factura Sencilla

---
+++ PrimerParcial/Ejercicios Tareas/Factura_Sencilla.vb
@@ -0,0 +1,21 @@
+﻿Public Class Factura_Sencilla
+    Private Sub btnCalcular_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCalcular.Click
+
+    End Sub
+
+    Private Sub btnSalir_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnSalir.Click
+
+    End Sub
+
+    Private Sub chkDescuento_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles chkDescuento.CheckedChanged
+
+    End Sub
+
+    Private Sub btnNuevo_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnNuevo.Click
+
+    End Sub
+
+    Private Sub chkISV_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles chkISV.CheckedChanged
+
+    End Sub
+End Class
